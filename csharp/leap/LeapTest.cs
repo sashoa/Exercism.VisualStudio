c0de402a4716886dc57f4b27cc753d69ceffe9c7
Sasho <sash239@gmail.com>
2017-03-02 16:23:39 +0100

    Add Leap Year Solution

--- csharp/leap/LeapTest.cs
+++ csharp/leap/LeapTest.cs
@@ -15,14 +15,12 @@ public class LeapTest
         Assert.That(Year.IsLeap(1997), Is.False);
     }
 
-    [Ignore("Remove to run test")]
     [Test]
     public void Turn_of_the_20th_century_is_not_a_leap_year()
     {
         Assert.That(Year.IsLeap(1900), Is.False);
     }
 
-    [Ignore("Remove to run test")]
     [Test]
     public void Turn_of_the_25th_century_is_a_leap_year()
     {
--- csharp/leap/Year.cs
+++ csharp/leap/Year.cs
@@ -2,7 +2,22 @@
 {
     public static bool IsLeap(int year)
     {
+        bool isDevisibleBy4 = year % 4 == 0;
+        bool isDevisibleBy100 = year % 100 == 0;
+        bool isDevisibleBy400 = year % 400 == 0;
 
-        return true;
+        if (isDevisibleBy4)
+        {
+            if (isDevisibleBy100)
+            {
+                if (isDevisibleBy400)
+                    return true;
+                else
+                    return false;
+            }
+            return true;
+        }
+        else
+            return false;
     }
 }
